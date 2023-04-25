0c5ec623a1905b3335a6abf4161811d09bf2d6cc
Enoch2k2 <enoch2k2@hotmail.com>
2023-04-25 15:21:03 -0500

    finished

--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,30 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
-    pass
+    def __init__(self, name='Guido', job='Sales'):
+        self.name = name
+        self.job = job
+
+    def set_name(self, name):
+        if (type(name) == str and 1 <= len(name) <= 25):
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_name(self):
+        return self._name
+
+    def set_job(self, job):
+        try:
+            APPROVED_JOBS.index(job)
+            self._job = job
+        except:
+            print("Job must be in list of approved jobs.")
+
+    def get_job(self):
+        return self._job
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
